aef22b4db01c17c4107a8b595a4ddbea5d0fdec7
RodoxReis7 <rodoxreis@gmail.com>
2025-01-09 00:09:12 -0300

    Revisão Aula 2

--- script-crud.js
+++ script-crud.js
@@ -23,10 +23,12 @@ function criarElementoTarefa(tarefa) {
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>`;
 
+  // Após criar o parágrafo
   const paragrafo = document.createElement("p");
   paragrafo.textContent = tarefa.descricao;
   paragrafo.classList.add("app__section-task-list-item-description");
 
+  // Após criar o botão
   const botao = document.createElement("button");
   botao.classList.add("app_button-edit");
 
@@ -68,6 +70,7 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
   formAdicionarTarefa.classList.add("hidden");
 });
 
+// Carregando e exibindo tarefas do localStorage ao carregar a página
 tarefas.forEach((tarefa) => {
   const elementoTarefa = criarElementoTarefa(tarefa);
   ulTarefas.append(elementoTarefa);
