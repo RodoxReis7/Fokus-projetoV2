98afc6caabf259119b8cefbd1ab005163b71a713
RodoxReis7 <rodoxreis@gmail.com>
2025-01-11 22:21:55 -0300

    Depurando código no navegador

--- script-crud.js
+++ script-crud.js
@@ -38,9 +38,12 @@ function criarElementoTarefa(tarefa) {
 
   botao.onclick = () => {
     const novaDescricao = prompt("Qual é o novo nome da tarefa?");
-    paragrafo.textContent = novaDescricao;
-    tarefa.descricao = novaDescricao;
-    atualizarTarefas();
+    console.log("Nova descrição da tarefa: ", novaDescricao);
+    if (novaDescricao) {
+      paragrafo.textContent = novaDescricao;
+      tarefa.descricao = novaDescricao;
+      atualizarTarefas();
+    }
   };
 
   const imagemBotao = document.createElement("img");
